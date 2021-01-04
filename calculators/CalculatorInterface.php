d1124f96db50c94730843d7e1abdbde4e075d4e4
Max <45854841+warendmir@users.noreply.github.com>
2021-01-04 14:54:38 +0300

    Update CalculatorInterface.php

--- calculators/CalculatorInterface.php
+++ calculators/CalculatorInterface.php
@@ -2,26 +2,60 @@
 
 namespace devanych\cart\calculators;
 
-interface CalculatorInterface
+class SimpleCalculator implements CalculatorInterface
 {
     /**
      * @param \devanych\cart\CartItem[] $items
      * @return integer
      */
-    public function getCost(array $items);
+    public function getCost(array $items)
+    {
+        $cost = 0;
+        foreach ($items as $item) {
+            $cost += $item->getCost();
+        }
+        return $cost;
+    }
+
     /**
      * @param \devanych\cart\CartItem[] $items
      * @return integer
      */
-    public function getOldPriceCost(array $items);
+    public function getOldPriceCost(array $items)
+    {
+        $cost = 0;
+        foreach ($items as $item) {
+            $cost += $item->getOldPriceCost();
+        }
+        return $cost;
+    }
     /**
      * @param \devanych\cart\CartItem[] $items
      * @return integer
      */
-    public function getPriceCostWithoutDiscount(array $items);
+    public function getPriceCostWithoutDiscount(array $items)
+    {
+        $cost = 0;
+        foreach ($items as $item) {
+            if ($item->getOldPriceCost()){
+                $cost += $item->getOldPriceCost();
+            } else {
+                $cost += $item->getCost();
+            }
+        }
+        return $cost;
+    }
+
     /**
      * @param \devanych\cart\CartItem[] $items
      * @return integer
      */
-    public function getCount(array $items);
+    public function getCount(array $items)
+    {
+        $count = 0;
+        foreach ($items as $item) {
+            $count += $item->getQuantity();
+        }
+        return $count;
+    }
 }
